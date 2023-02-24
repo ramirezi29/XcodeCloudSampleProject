4c2b0041b702bec2cf0ae6837052015193e9c127
ivan <iramirez22ios@gmail.com>
2023-02-24 12:43:19 -0600

    Initial Commit

---
+++ XcodeCloudSampleProject/XcodeCloudSampleProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeCloudSampleProjectApp.swift
+//  XcodeCloudSampleProject
+//
+//  Created by Ivan Ramirez on 2/24/23.
+//
+
+import SwiftUI
+
+@main
+struct XcodeCloudSampleProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
